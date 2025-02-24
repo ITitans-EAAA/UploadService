959723d72c32ee92a1d6d2558dc4f7a894ac1f7d
HjalteHH <hjal0550@skivecollege.dk>
2025-02-24 13:23:22 +0100

    initial commit

---
+++ upload-service/uploadServiceAPI.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace uploadServiceAPI.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
